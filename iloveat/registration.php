13d61016828ea2ff5676dd85fabd1ae12cda96ce
Milven Domingos <milven-cossa@hotmail.com>
2018-05-15 13:41:05 +0200

    first commit

---
+++ iloveat/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milven
+ * Date: 5/15/18
+ * Time: 11:27 AM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/boldcom/iloveat',
+    __DIR__
+);
